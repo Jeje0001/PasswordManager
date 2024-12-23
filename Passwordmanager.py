d5777656449f0f0739c9f634e794375555d4221f
jejea <157323284+Jeje0001@users.noreply.github.com>
2024-12-23 16:52:40 -0500

    changes

--- Passwordmanager.py
+++ Passwordmanager.py
@@ -1,16 +1,29 @@
-pwd=input("What is the master password")
+master_pwd=input("What is the master password")
+
+
 def view():
-    pass
+    with open("passwords.txt","r") as f:
+        for line in f.readlines():
+            data=line.rstrip()
+            user,passw= data.split("|")
+            print("Username: "+ user, "Password: "+ passw )
+            
+                   # print(f.read())
 def add():
-    pass
+    name=input("Account name:")
+    pwd=input("Password: ")
+
+    with open("passwords.txt","a") as f:
+        f.write(name + "|" + pwd + "\n")
+
 while True:
     mode=input("Do you want to add a new password or view a exis password(Enter view to view or Enter add to add or Enter q to quit)").lower()
     if mode == "q":
         break
     if mode == "view":
-        pass
+        view()
     elif mode == "add":
-        pass
+        add()
     else:
         print("Invalid Input")
         continue
