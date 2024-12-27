9f0a1efdf25120697616ee49ca6b4a26c8aa5d8d
jejea <157323284+Jeje0001@users.noreply.github.com>
2024-12-27 14:11:31 -0500

    changes

--- Passwordmanager.py
+++ Passwordmanager.py
@@ -1,4 +1,19 @@
+from cryptography.fernet import Fernet
 master_pwd=input("What is the master password")
+
+def viewkey():
+    file=open("key.key","rb")
+    key=file.read()
+    file.close()
+    return key
+key=viewkey() + master_pwd.encode()
+fer=Fernet(key)
+
+# def createkey():
+#     key=Fernet.generate_key()
+
+#     with open("key.key","wb") as key_file:
+#         key_file.write(key)
 
 
 def view():
@@ -6,7 +21,7 @@ def view():
         for line in f.readlines():
             data=line.rstrip()
             user,passw= data.split("|")
-            print("Username: "+ user, "Password: "+ passw )
+            print("Username: "+ user, "Password: "+ str(fer.decrypt(passw)))
             
                    # print(f.read())
 def add():
@@ -14,7 +29,7 @@ def add():
     pwd=input("Password: ")
 
     with open("passwords.txt","a") as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 while True:
     mode=input("Do you want to add a new password or view a exis password(Enter view to view or Enter add to add or Enter q to quit)").lower()
@@ -27,3 +42,5 @@ while True:
     else:
         print("Invalid Input")
         continue
+
+#Continue at 1:34:20
